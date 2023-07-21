8112f8974f88e99ca17a0f10bf67e3b775821327
beba2123 <Antenehbeba@gmail.com>
2023-07-21 15:01:40 +0300

    Teacher table

--- school/models.py
+++ school/models.py
@@ -11,5 +11,11 @@ class Student(models.Model):
     sex        = models.CharField(max_length=1, null = False, blank= False)
     class_room_id = models.ForeignKey()
 
-class Family(models.Model):
-     id  = models.UUIDField(default=uuid.uuid4, unique= True, primary_key= True, editable= False)
+class Teacher(models.Model):
+     id   = models.UUIDField(default=uuid.uuid4, unique= True, primary_key= True, editable= False)
+     First_Name = models.CharField(max_length=200, null = False, blank = False)
+     Middle_Name = models.CharField(max_length=200, null=True, blank=True)
+     Last_Name = models.CharField(max_length=200, null=True, blank=True)
+     Date_Of_Birth = models.DateField(max_length=200, null=True, blank=True)
+     email = models.EmailField(max_length=200, null=True, blank=True)
+     Phone_Number = models.IntegerField(max_length=200, null=True, blank=True)
